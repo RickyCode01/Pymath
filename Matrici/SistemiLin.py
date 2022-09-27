983ac116cc096e2ffdcd565e7c76c6516017283d
RickyCode01 <riccardoManoni7@gmail.com>
2022-09-27 11:20:50 +0200

    bug fix zero division in sis lin

--- Matrici/SistemiLin.py
+++ Matrici/SistemiLin.py
@@ -1,7 +1,9 @@
 # modulo soluzioni sistemi lineari - Riccardo Manoni
 
+import Determinante
+import Matrice
 from Inversa import MontanteBareiss
-import Matrice, Determinante
+
 
 def ChangeColn(matrix, index, vector):
     m2 = [row[:] for row in matrix]
@@ -14,11 +16,15 @@ def ChangeColn(matrix, index, vector):
 def Cramer(A, B): # A = matrice coefficienti, B = vettore termini noti
     solutions = []
     Det = Determinante.DetMatrice(A,0,0)
-    for i in range(len(A)): # indice di dimensione matrice
-        # determino le soluzioni date dalla Matrice dei coeff modificata / determinante
-        #Matrice.printMatrix(ChangeColn(A, i, B))
-        solutions.append(Determinante.DetMatrice(ChangeColn(A, i, B),0,0)/Det)
-    return solutions
+    if Det != 0:
+        for i in range(len(A)): # indice di dimensione matrice
+            # determino le soluzioni date dalla Matrice dei coeff modificata / determinante
+            #Matrice.printMatrix(ChangeColn(A, i, B))
+            solutions.append(Determinante.DetMatrice(ChangeColn(A, i, B),0,0)/Det)
+            return solutions
+    else:
+        return -1
+    
 
 def InitSis():
     eq = input("Insert number of equations: ")
@@ -43,9 +49,11 @@ def CalcSol(m):
             a = Matrice.SubMatrix(m, 0, x, 0, y-1)
             b = Matrice.SubMatrix(m, 0, x, y-1, y)
             sol = Cramer(a, b)
-            for i in range(len(sol)):
-                print("x" + str(i) + ": " + str(sol[i]))
-
+            if  sol != -1:
+                for i in range(len(sol)):
+                    print("x" + str(i) + ": " + str(sol[i]))
+            else:
+                print("No solutions")
 
 CalcSol(InitSis())
 #print(ChangeColn([[0,2,3],[1,5,6],[1,8,9]], 1, [1,2,3]))
